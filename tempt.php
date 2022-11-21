378d05e6a165ebb37081c84d1711205a67a09e2b
jakesebastian-forbes <jforbes557@gmail.com>
2022-11-21 21:30:49 +0800

    shit

---
+++ tempt.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php
+require('must_haves.php');
+
+?>
+</head>
+<body>
+<div class="container-fluid">
+
+<?php 
+      $title_sidebar = "MY PROFILE";
+      include('client_sidebar.php');?>
+
+
+</div>
+
+</body>
+</html>
