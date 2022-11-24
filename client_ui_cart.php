5e81afe1e37fa3373f1dcf000ae54078b56ef544
jakesebastian-forbes <jforbes557@gmail.com>
2022-11-25 07:00:28 +0800

    nov 25 morning

--- client_ui_cart.php
+++ client_ui_cart.php
@@ -17,28 +17,28 @@ include 'db_connection.php';
     <script src="https://kit.fontawesome.com/1c020da525.js" crossorigin="anonymous"></script> 
     <script data-require="jquery@3.1.1" data-semver="3.1.1" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script>
-        function wcqib_refresh_quantity_increments() {
-    jQuery("div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)").each(function(a, b) {
-        var c = jQuery(b);
-        c.addClass("buttons_added"), c.children().first().before('<input type="button" value="-" class="minus" />'), c.children().last().after('<input type="button" value="+" class="plus" />')
-        })
-        }
-        String.prototype.getDecimals || (String.prototype.getDecimals = function() {
-            var a = this,
-                b = ("" + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
-            return b ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0)) : 0
-        }), jQuery(document).ready(function() {
-            wcqib_refresh_quantity_increments()
-        }), jQuery(document).on("updated_wc_div", function() {
-            wcqib_refresh_quantity_increments()
-        }), jQuery(document).on("click", ".plus, .minus", function() {
-            var a = jQuery(this).closest(".quantity").find(".qty"),
-                b = parseFloat(a.val()),
-                c = parseFloat(a.attr("max")),
-                d = parseFloat(a.attr("min")),
-                e = a.attr("step");
-            b && "" !== b && "NaN" !== b || (b = 0), "" !== c && "NaN" !== c || (c = ""), "" !== d && "NaN" !== d || (d = 0), "any" !== e && "" !== e && void 0 !== e && "NaN" !== parseFloat(e) || (e = 1), jQuery(this).is(".plus") ? c && b >= c ? a.val(c) : a.val((b + parseFloat(e)).toFixed(e.getDecimals())) : d && b <= d ? a.val(d) : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())), a.trigger("change")
-        });
+    //     function wcqib_refresh_quantity_increments() {
+    // jQuery("div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)").each(function(a, b) {
+    //     var c = jQuery(b);
+    //     c.addClass("buttons_added"), c.children().first().before('<input type="button" value="-" class="minus" />'), c.children().last().after('<input type="button" value="+" class="plus" />')
+    //     })
+    //     }
+    //     String.prototype.getDecimals || (String.prototype.getDecimals = function() {
+    //         var a = this,
+    //             b = ("" + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+    //         return b ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0)) : 0
+    //     }), jQuery(document).ready(function() {
+    //         wcqib_refresh_quantity_increments()
+    //     }), jQuery(document).on("updated_wc_div", function() {
+    //         wcqib_refresh_quantity_increments()
+    //     }), jQuery(document).on("click", ".plus, .minus", function() {
+    //         var a = jQuery(this).closest(".quantity").find(".qty"),
+    //             b = parseFloat(a.val()),
+    //             c = parseFloat(a.attr("max")),
+    //             d = parseFloat(a.attr("min")),
+    //             e = a.attr("step");
+    //         b && "" !== b && "NaN" !== b || (b = 0), "" !== c && "NaN" !== c || (c = ""), "" !== d && "NaN" !== d || (d = 0), "any" !== e && "" !== e && void 0 !== e && "NaN" !== parseFloat(e) || (e = 1), jQuery(this).is(".plus") ? c && b >= c ? a.val(c) : a.val((b + parseFloat(e)).toFixed(e.getDecimals())) : d && b <= d ? a.val(d) : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())), a.trigger("change")
+    //     });
     </script>
 
 
@@ -184,7 +184,7 @@ PLACE ORDER
             if($conn->connect_error){
                 die('Connection failed : ' . $conn->connect_error);
             }else{
-                // echo " uyyy". $category;
+
             $query = "SELECT * FROM `my_cart` WHERE `client_id` = $client_id ORDER BY date_added DESC;";
 
             $result_orderID = mysqli_query($conn,$query);
@@ -194,20 +194,22 @@ PLACE ORDER
             
             while($rows = mysqli_fetch_assoc($result_orderID))
                 {
-                // $order_id =  $rows_orderID['order_id']
+         
             ?>
 
             
-                <ul class="itemRow" id="<?php echo  'cart_id_'.$rows['cart_id']?>" >
+                <ul class="itemRow" id="<?php echo 'cart_id_'.$rows['cart_id']?>" >
                 
                     <div class="row m-0">
                         <div class="col-sm-3" style="align-content:center">
-                        <input type="text" value = "<?php echo  $rows['menu_id']?>" hidden>
-                            <input type="checkbox" id="<?php echo  $rows['cart_id']?>" 
-                            name="item1" value="<?php echo $rows['item_name']?>
-                            "style="width:20px;height:20px; margin-right: 10px;" 
-                            onclick = "ischecked(this.id,this.value)"
-                            class = "my-checkbox">
+              
+                            <input type="checkbox" 
+                            class = "my-checkbox"
+                            style="width:20px;height:20px; margin-right: 10px;" 
+                            id="<?php echo  $rows['menu_id']?>" 
+                            name="item1" value="<?php echo $rows['item_name']?>"
+                            onclick = "ischecked(this.value,this.id)"
+                            >
                          
                          <?php  echo '<img src=" data:image/jpeg;base64,'.base64_encode($rows['image']) .'" 
                          alt="corndog" width="100" height="100">' ?>
@@ -220,9 +222,21 @@ PLACE ORDER
                            <h5 style="margin-top: 20px;" ><?php echo $rows['item_price']?></h5>
                         </div>
                         <div class="col-sm-2">
+                            
                             <div class="quantity quantity_btn" style="display: flex; margin-top: 20px;">       
-                                <input type="number" step="1" min="1" max="" name="quantity" value="<?php echo $rows['qnt']?>" title="Qty" class="input-text qty text" size="40" pattern="" inputmode="" style="width: 40px; ">       
-                            </div>
+                            <input type="button" value="-" class="minus" name = <?php echo  $rows['menu_id']?> 
+                            onclick = "less(this.name)">
+                            
+                            <input type="number" step="1" min="1" max="" name="quantity" value="<?php echo $rows['qnt']?>"
+                                 title="Qty" class="input-text qty text" size="40" pattern="" inputmode="" style="width: 40px;"
+                                id = "qnty_counter_<?php echo  $rows['menu_id']?>"
+                                 >       
+                                 <input type="button" value="+" class="plus" 
+                                 name = <?php echo  $rows['menu_id']?>
+                                 onclick = "more(this.name)">
+                            
+                                </div>
+                            
                         </div>
                         <div class="col-sm-2">
                             <h5 style="margin-top: 20px;"><?php echo $rows['item_price'] * $rows['qnt']?></h5>    
@@ -274,7 +288,7 @@ PLACE ORDER
   </div>
 </div>
 
-<div class = "row"
+<div class = "row overflow-auto"
 style = "background-color:#c1bdbd; 
               border:1px solid gray;
               min-width:320px;
@@ -289,40 +303,92 @@ style = "background-color:#c1bdbd;
 
 </div>
 
+<div id = "total_price">
+
+</div>
+
 </div>
 
 
 <script>
 var active = document.getElementById('nav_cart');
 active.setAttribute('class','nav-link my-nav-link my-active',);
+var my_total_price = 0;
 
-function ischecked(id,name){
-
+function ischecked(item_name,id){
 //    var id = '#' + id;
 var chk = $("#"+id).is(":checked");
+
    if($('#' + id).is(":checked")) {
 
-        console.log(chk + " :"+ id);
+        console.log(chk + " :"+ item_name + "name" +"sess" + <?php echo $_SESSION['client_id']?>);
 
-    $("ul").filter("#cart_id_"+id).css("color", "yellow");
+        $("ul").filter("#cart_id_"+id).css("color", "yellow");
 
-    $("#total_preview").append("<h6 class = cart_"+id+">"+name+"</h6>");
+        // $("#total_preview").append("<h6 class = cart_"+menu_id+">"+menu_id +"asda"+"</h6>");
+
+        $.post("select_items.php",
+    {
+        item_name: item_name,
+        client_id: <?php echo $_SESSION['client_id']?>
+    },
+    function(data, status){
+        myArr = data.split("--");
+        // alert("Data: " + data + "\nStatus: " + status);
+        var cart_id = "cart_id_" +id;
+        $("#total_preview").append("<div class = 'row' id ="+cart_id+ ">"+
+           "<h6 class = 'col mx-auto'>" + myArr[0]+ "</h6>" +
+           "<h6 class = 'col'>" + myArr[1]+ "</h6>" +
+           "<h6 class = 'col'>" + myArr[2]+ "</h6>" +
+           "<h6 class = 'col' id = total_"+id+">" + myArr[3]+ "</h6>" 
+        +"</div>");
+
+        // console.log($('h6').html);
+        // console.log(typeof(data));
+        my_total_price += parseFloat($("#total_" + id).text());
+
+        $("#total_price").text(my_total_price);
+        // console.log(typeof(myArr));
+        // console.log(myArr[2]);
+
+    });
 
    }else{
     // console.log("Not checked");
-    console.log(chk+ " :"+ id);
+    console.log(chk+ " :"+ item_name);
     
-    $("h6").remove(".cart_"+id);
-    $("ul").filter("#cart_id_"+id).css("color", "black");
+    // $("div").remove("#cart_id_"+id);
+    
+    my_total_price -= parseFloat($("#total_" + id).text());
 
+$("#total_price").text(my_total_price);
+    $("#cart_id_" + id).remove();
+    $("ul").filter("#cart_id_"+id).css("color", "black");
 
    }
 
+}
 
-    
 
-//    $(id).css("color", "yellow");
+function more(id){
+    // console.log(id);
+    var qnty = parseInt($("#qnty_counter_"+id).val());
+    // console.log("q" + qnty);
+    $("#qnty_counter_"+id).val(qnty+1);
+// console.log($("#qnty_counter_"+id).val(qnty+1));
 
+}
+
+function less(id){
+
+    var qnty = parseInt($("#qnty_counter_"+id).val());
+    // console.log("q" + qnty);
+    if(qnty <= 0){
+
+    }else{
+        $("#qnty_counter_"+id).val(qnty-1);
+    }
+    // $("#qnty_counter_"+id).val(qnty-1);
 }
 
 $(document).ready(function(){
--- select_items.php
+++ select_items.php
@@ -1,4 +1,33 @@
 <?php
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "restomatic_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$item_name = $_POST['item_name'];
+$client_id = $_POST['client_id'];
+
+$sql = "SELECT * FROM `my_cart` WHERE item_name = '$item_name' AND client_id = '$client_id' ;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo $row['item_name'] . "--" .  $row['item_price'] ."--". $row['qnt'] . "x--". $row['item_price'] * $row['qnt'] ;
+
+
+}
+} else {
+  echo "0 results";
+}
+$conn->close();
 
 ?>
